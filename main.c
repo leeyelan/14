679349fe77485863f63a91383d17ec949d2f5786
leeyelan <yr00868@sookmyung.ac.kr>
2020-12-03 15:43:11 +0900

    2nd

--- main.c
+++ main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Book{
+ 	int number;
+ 	char title[20];
+ };
+
+
 void main(void){
-	char *pc = NULL;
-	int i = 0;
+	struct Book *p;
 	
-	pc = (char*)malloc(100*sizeof(char));
-	if(pc==NULL){
+	p = (struct Book*)malloc(2*sizeof(struct Book));
+	
+	if(p == NULL){
 		printf("ERROR of memory\n");
-		exit(1);
+		return;
 	}
 	
-	for(i=0; i<26; i++){
-		pc[i] = 'a'+i;
-	}
+	p -> number = 1;
+	strcpy(p->title, "C programming");
 	
-	pc[i] = 0;
-	printf("%s\n",pc);
-	free(pc);
+	(p+1) -> number = 2;
+	strcpy((p+1)->title, "Electronics");
+	
+	printf("%s %s\n", p->title, (p+1)->title);
+	
+	free(p);
+	return;
+
+ 
 }
